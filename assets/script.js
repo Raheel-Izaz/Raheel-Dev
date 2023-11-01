eb7acb565abcd34b58f90e7a49b9b7865227d582
Raheel-Izaz <raheelizaz3@gmail.com>
2023-11-01 18:38:05 +0500

    Raheel Portfoloi

---
+++ assets/script.js
@@ -0,0 +1,130 @@
+
+// Navar sticky on scroll
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY >= 1){
+        header.classList.add("sticky");
+    }else{
+        header.classList.remove("sticky")
+    }
+})
+
+
+
+// Toggle Menu Button on Navbar 
+let menu = document.querySelector('#bar');
+let navList = document.querySelector('.nav-list')
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-xmark')
+    navList.classList.toggle('open')
+}
+
+const sr = ScrollReveal ({
+    distance:'50px',
+    duration:2050,
+    reset:false
+});
+
+// sr.reveal('.logo',{delay:10,});
+// sr.reveal('.contact-me',{delay:10,});
+// sr.reveal('.navbar',{delay:10,});
+sr.reveal('.details span.first',{delay:300,origin:'bottom'});
+sr.reveal('.details h1',{delay:400,origin:'left'});
+sr.reveal('.details h2',{delay:500,origin:'right'});
+sr.reveal('.details p',{delay:600,origin:'left'});
+sr.reveal('.social-icons',{delay:700,origin:'right'});
+sr.reveal('.talk-me-btn',{delay:700,origin:'bottom'});
+sr.reveal('.scroll a',{delay:850,origin:'top'});
+sr.reveal('.image img',{delay:900,origin:'right'});
+
+
+// Services Slider
+
+
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    // Records the initial cursor and scroll position of the carousel
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return; // if isDragging is false return from here
+    // Updates the scroll position of the carousel based on the cursor movement
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const infiniteScroll = () => {
+    // If the carousel is at the beginning, scroll to the end
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+
+    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+}
+
+const autoPlay = () => {
+    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+    // Autoplay the carousel after every 2500 ms
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+}
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
